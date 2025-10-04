bb707da90b58764cb1f9e7b1e103bd5188bcaf9c
Fabio Coimbra <fabiogc1989@gmail.com>
2025-10-04 16:33:31 +0100

    Add Models to admin section

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Position, Employee, Service
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active', 'modifiedOn')
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'icon', 'active', 'modifiedOn')
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'active', 'modifiedOn')

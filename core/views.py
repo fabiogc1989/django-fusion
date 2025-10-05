d5cac8a4e043c03e125caf282e4c0da4bfd154bb
Fabio Coimbra <fabiogc1989@gmail.com>
2025-10-05 13:39:45 +0100

    Index View with context

--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
+from .models import Service, Employee
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['services'] = Service.objects.order_by('?').all()
+        context['employees'] = Employee.objects.order_by('?').all()
+        return context
+        
